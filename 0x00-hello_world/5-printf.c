6b4ab594aba637c524e491b09e9599c8429fcdc6
Anorkor <sarleeabb@gmail.com>
2022-04-08 03:16:07 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * * main - Function begins
+ * *
+ * * Return: Function ends
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}

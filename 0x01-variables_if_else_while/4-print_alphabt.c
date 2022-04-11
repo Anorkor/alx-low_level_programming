ef2f66607d8517b275c64d70048a509c71930bac
Anorkor <sarleeabb@gmail.com>
2022-04-11 08:19:56 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet == 'q')
 			continue;
-		
 		else if (alphabet == 'e')
 			continue;
 

2c9d1772c261ea9dad25a30b4e837b6acab80eb4
S. Anorkor Abbey <sarleeabb@gmail.com>
2022-04-22 16:40:58 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,21 +2,20 @@
 
 /**
  * print_rev - check the code
- * @s: variable
+ * @s: variable.
  */
-
 void print_rev(char *s)
 {
-	int i;
-	int a;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		continue;
-	}
-	i--;
-	for (a = 0; a <= i; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+int i;
+int a;
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+i--;
+for (a = 0; a <= i; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }

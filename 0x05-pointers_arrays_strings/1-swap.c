5edf850a6cb361869cfe221478151bb1d260a331
S. Anorkor Abbey <sarleeabb@gmail.com>
2022-04-22 16:32:06 +0000

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * swap_int - check the code
- * @a: variable
+ * swap_int - check the code for Holberton School students.
+ * @a: variable.
  * @b: variable
  */
 
 void swap_int(int *a, int *b)
 {
-	int temp;
-	temp = *b;
-	*b = *a;
-	*a = temp;
+int temp;
+temp = *b;
+*b = *a;
+*a = temp;
 }

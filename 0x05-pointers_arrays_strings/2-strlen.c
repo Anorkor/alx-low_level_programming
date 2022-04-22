b6226bc3cba28ab00d76d058dbdc56d5d1f76ef9
Anorkor <sarleeabb@gmail.com>
2022-04-22 05:49:50 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ * @s: a variable
+ *
+ * return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return(i);
+}

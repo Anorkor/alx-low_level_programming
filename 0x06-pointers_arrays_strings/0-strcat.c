72b0394271312c5e067d896e611a298bdaa5280d
S. Anorkor Abbey <sarleeabb@gmail.com>
2022-04-23 15:58:43 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "hold.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,18 +7,15 @@
  * Return:concatenated string
  */
 
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i, a;
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (a = 0; src[a] != '\0'; a++)
-	{
-		dest[i] += src[a];
-		i++;
-	}
-	dest += '\0';
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }

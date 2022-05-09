f8517d6f6211454a4f0d0bc769e5b5ebf45c067a
S. Anorkor Abbey <sarleeabb@gmail.com>
2022-05-09 15:53:57 +0000

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 /**
-  * string_nconcat - check if malloc library is assigning memories
-  * @s1: variable
-  * @s2: parameter 2
-  * @n : n charachter
-  * Return: integer
-  */
-
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: limit of s2
+ * Return: pointer to new space in memory or null
+ **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *concat_str;
-unsigned int concat_index = 0, lens2 = 0, lens1 = 0, idx = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (; s2[lens1] != '\0'; lens1++)
-;
-for (; s2[lens2] != '\0'; lens2++)
-;
-if (n > lens2)
-concat_str = malloc(sizeof(char *) * (lens1 + lens2 + 1));
-else
-concat_str = malloc(sizeof(char) * (lens1 + n + 1));
-if (concat_str == NULL)
-return (NULL);
-while (s1[concat_index] != '\0')
-{
-concat_str[concat_index] = s1[concat_index];
-concat_index++;
-}
-if (lens2 <= n)
-{
-while (s2[idx] != '\0')
-{
-concat_str[concat_index++] = s2[idx];
-idx++;
-}
-}
-else
-{
-while (idx < n && s2[idx] != '\0')
-{
-concat_str[concat_index] = s2[idx++];
-concat_index++;
-}
-}
-concat_str[concat_index] = '\0';
-return (concat_str);
+	char *strDup;
+	int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	strDup = malloc(sizeof(char) * (i + n + 1));
+	if (strDup == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		strDup[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		strDup[i] = s2[j];
+		i++, j++;
+	}
+	strDup[i] = '\0';
+	return (strDup);
 }
